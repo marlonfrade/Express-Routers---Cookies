5dc619ec526867caac92f9e2d2a6a1eea1c10729
Marlon Frade <marlonmelo98@hotmail.com>
2022-02-22 01:48:29 -0300

    implement the cookies-parser on the app.js file

--- Comments/CookiesDemo/app.js
+++ Comments/CookiesDemo/app.js
@@ -2,12 +2,36 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// npm i cookie-parser
+const cookieParser = require("cookie-parser");
+//Execute it
+app.use(cookieParser());
+
 // We configure the route and the information we want to store
 
 // Route to test
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// We're creating this third router to show how to use cookie-parser that we install
+app.get("/greet", (req, res) => {
+  // After we install cookie-parser, we'll be able to use req.cookies to get the cookies stored in the browser
+  //   We can see it using a console.log
+  //   console.log(req.cookies);
+  // Use this console.log above to test if the cookie is available
+
+  // Now we destructuring the cookie
+  //   And in case there's no name, we can set a pattern value for it using the = signal
+  const { name = anonymous } = req.cookies;
+
+  res.send(`Hey There, ${name}`);
+});
+// To test this third route above, go to /setName, verify if the cookie has been sent to the application devtool
+// Then go back to /greet and verify on the console if there's a response Object with the cookie we'd stored
+
+// If you can't store the cookie using the application, open the cookies on devtool and add a new cookie by your own,
+// Then go back to /greet and the cookie you added must be available on the console
 
 //Using the express documentation about cookies, we can store information using the logic below
 app.get("/setName", (req, res) => {
